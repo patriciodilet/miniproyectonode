a9e89f84789c74b8da1367934451a26855dcc8da
Patricio Diaz <patricio.dilet@gmail.com>
2017-10-02 02:56:20 -0300

    branch add

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // Inicialización
-var express  = require('express');
-var app      = express(); 							// Utilizamos express
-var mongoose = require('mongoose'); 				// mongoose para mongodb
-var port  	 = process.env.PORT || 8080; 			// definimos puerto 8080
+var express = require('express');
+var app = express(); // Utilizamos express
+var mongoose = require('mongoose'); // mongoose para mongodb
+var port = process.env.PORT || 3000; // definimos puerto 8080
 
 // Configuracion
-mongoose.connect('mongodb://localhost:27017/EjemploMEAN'); 	// Conexión a la base de datos de Mongo con nombre "MeanExample"
+mongoose.connect('mongodb://localhost:27017/EjemploMEAN'); // Conexión a la base de datos de Mongo con nombre "MeanExample"
 
 app.configure(function() {
-	app.use(express.static(__dirname + '/angular'));
-	app.use(express.logger('dev')); 						// activamos el log en modo 'dev'
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/angular'));
+    app.use(express.logger('dev')); // activamos el log en modo 'dev'
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
 });
 
 // rutas
