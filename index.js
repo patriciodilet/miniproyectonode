c036a9257b1faff9869a4e9c63d1b5f19dc50ece
Patricio Diaz <patricio.dilet@gmail.com>
2017-10-02 03:06:15 -0300

    db

---
+++ index.js
@@ -0,0 +1,25 @@
+// Inicialización
+var express = require('express');
+var app = express(); // Utilizamos express
+var mongoose = require('mongoose'); // mongoose para mongodb
+var port = process.env.PORT || 3000; // definimos puerto 8080
+
+// Configuracion
+//mongodb://dbuser:asdj6##f2##fsaA@ds157614.mlab.com:57614/heroku_5f4h5tmx
+// asdj6##f2##fsaA
+//mongoose.connect('mongodb://localhost:27017/EjemploMEAN'); // Conexión a la base de datos de Mongo con nombre "MeanExample"
+mongoose.connect('mongodb://dbuser:asdj6##f2##fsaA@ds157614.mlab.com:57614/heroku_5f4h5tmx');
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/angular'));
+    app.use(express.logger('dev')); // activamos el log en modo 'dev'
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+
+// rutas
+require('./app/routes.js')(app);
+
+// puerto que escuchara
+app.listen(port);
+console.log("La APP se encuentra escuchando por el puerto " + port);
